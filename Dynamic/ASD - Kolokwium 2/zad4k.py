8a2d6734233de0890df3efc0c407017bea4b0bc8
Sn3cor <jakub.krupa23@gmail.com>
2025-06-04 01:10:46 +0200

    dynamic

--- Dynamic/ASD - Kolokwium 2/zad4k.py
+++ Dynamic/ASD - Kolokwium 2/zad4k.py
@@ -1,7 +1,38 @@
 from zad4ktesty import runtests
 
 def falisz ( T ):
-    #Tutaj proszę wpisać własną implementację
-    return 0
+    
+
+    def rek(T,i,j):
+        n = len(T)
+        if i==n-1 and j == n-1:
+            return T[n-1][n-1]
+        elif i+1<n and j +1< n:
+            return min(T[i][j] + rek(T,i+1,j), T[i][j] + rek(T,i,j+1))
+        elif i+1>=n:
+            return min(T[i][j] + float("inf"), T[i][j] + rek(T,i,j+1))
+        elif j+1>=n:
+            return min(T[i][j] + rek(T,i+1,j), T[i][j] + float("inf"))
+    return rek(T,0,0)
+
+
+def falisz(T):
+    n = len(T)
+    dp = [[float("inf") for _ in range(n)] for _ in range(n)]
+
+    dp[n-1][n-1] = 0
+
+    for i in range(n-1,-1,-1):
+        for j in range(n-1,-1,-1):
+            if i+1>=n and j+1>=n:
+                continue
+            elif i+1>=n and j+1<n:
+                dp[i][j] = T[i][j] + min(float("inf"),dp[i][j+1])
+            elif i+1<n and j+1>=n:
+                dp[i][j] = T[i][j] + min(float("inf"),dp[i+1][j])
+            else:
+                dp[i][j] = T[i][j] + min(dp[i+1][j],dp[i][j+1])
+
+    return dp[0][0]
 
 runtests ( falisz )
--- Dynamic/ASD - Kolokwium 2/zad5k.py
+++ Dynamic/ASD - Kolokwium 2/zad5k.py
@@ -1,7 +1,13 @@
 from zad5ktesty import runtests
 
 def garek ( A ):
-    #Tutaj proszę wpisać własną implementację
-    return 0
+    # n= len(A)
+    # def rek(A,i,j):
+    #     if i+1 == j:
+    #         return max(A[i],A[j])
+        
+    #     return max(A[i+1]+rek(A,i+1,j-1),A[j-1]+rek(A,i+1,j-1))
 
+    # return rek(A,0,n-1)
+    pass
 runtests ( garek )
