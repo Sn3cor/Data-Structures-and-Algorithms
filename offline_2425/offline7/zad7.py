8e07175b8fb5f6092c5739612ce42b6c03b641ac
Sn3cor <jakub.krupa23@gmail.com>
2025-06-11 22:17:49 +0200

    cos

--- offline_2425/offline7/zad7.py
+++ offline_2425/offline7/zad7.py
@@ -2,9 +2,7 @@ from zad7testy import runtests
 
 
 def orchard(T, m):
-    # tu prosze wpisac wlasna implementacje
-    return -1
-
+ pass
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests(orchard, all_tests=False)
